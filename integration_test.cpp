688550a9b4cc1e97931df63f2b3bf009e23374ac
Dmitry I. Lyakh <quant4me@gmail.com>
2020-01-15 15:34:04 -0500

    Added comments, fixed make.sh, and added .gitignore.

--- integration_test.cpp
+++ integration_test.cpp
@@ -1,35 +1,41 @@
 #include "xacc.hpp"
 
 int main(int argc, char** argv) {
-    xacc::Initialize(argc, argv);
-    
-    auto qpu = xacc::getAccelerator("tnqvm");//, {std::make_pair("tnqvm-visitor", "exatn")});
 
-    auto xasmCompiler = xacc::getCompiler("xasm");
-    auto ir = xasmCompiler->compile(R"(__qpu__ void ansatz(qbit q, double theta) {
-      X(q[0]);
-      Ry(q[1], theta);
-      CX(q[1], q[0]);
-      H(q[0]);
-      H(q[1]);
-      Measure(q[0]);
-      Measure(q[1]);
-    })", qpu);
-    auto circuit = ir->getComposite("ansatz");
+  //Initialize the XACC runtime:
+  xacc::Initialize(argc, argv);
 
-    auto angles = xacc::linspace(-3.1415, 3.1415, 20);
+  //Choose the desired quantum accelerator:
+  auto qpu = xacc::getAccelerator("tnqvm");//, {std::make_pair("tnqvm-visitor", "exatn")});
 
-    for (auto& a : angles) {
+  //Choose the desired quantum programming language:
+  auto xasmCompiler = xacc::getCompiler("xasm");
 
-        auto evaled = (*circuit)({a});
+  //Compile a quantum kernel into the quantum IR:
+  auto ir = xasmCompiler->compile(
+  R"(__qpu__ void ansatz(qbit q, double theta) {
+    X(q[0]);
+    Ry(q[1], theta);
+    CX(q[1], q[0]);
+    H(q[0]);
+    H(q[1]);
+    Measure(q[0]);
+    Measure(q[1]);
+  })", qpu);
 
-        auto qubits = xacc::qalloc(2);
-        qpu->execute(qubits, evaled);
+  //Get the generated parameterized quantum circuit:
+  auto circuit = ir->getComposite("ansatz");
 
-        auto exp_val = qubits->getExpectationValueZ();
-        std::cout << "<X0X1>(" << a << ") = " << exp_val << "\n";
-    }
+  //Perform quantum/classical computation:
+  auto angles = xacc::linspace(-3.1415, 3.1415, 20);
+  for (auto & a : angles) {
+    auto evaled = (*circuit)({a});
+    auto qubits = xacc::qalloc(2);
+    qpu->execute(qubits, evaled);
+    auto exp_val = qubits->getExpectationValueZ();
+    std::cout << "<X0X1>(" << a << ") = " << exp_val << "\n";
+  }
 
-    xacc::Finalize();
-
+  //Finalize the XACC runtime:
+  xacc::Finalize();
 }
