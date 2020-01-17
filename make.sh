2a364f93c01c462a7758a352a551d0f71826887b
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-01-17 14:00:23 -0500

    uploading make.sh with intel mkl rpath
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 g++ -std=c++14 -g -I $HOME/.xacc/include/xacc -c integration_test.cpp -o integration_test.o
-g++ integration_test.o -rdynamic -Wl,-rpath,$HOME/.xacc/lib -L $HOME/.xacc/lib -lxacc -lCppMicroServices -o integration_test.x
+g++ integration_test.o -rdynamic -Wl,-rpath,$HOME/.xacc/lib:$HOME/intel/mkl/lib/intel64 -L $HOME/.xacc/lib -lxacc -lCppMicroServices -o integration_test.x
