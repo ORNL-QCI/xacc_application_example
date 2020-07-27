8fcdc50d88604fb294eae7d5806312bfa0ed8f8e
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-07-27 13:47:46 -0400

    removing exatn config calls from xacc build, which uses exatn via the plugin interface
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- make_xacc_app.sh
+++ make_xacc_app.sh
@@ -1,3 +1,3 @@
 rm *.o *.x
-g++ `$HOME/.exatn/bin/exatn-config --cxxflags` `$HOME/.exatn/bin/exatn-config --includes` -std=c++17 -I $HOME/.xacc/include/xacc -c xacc_app.cpp -o xacc_app.o
-g++ xacc_app.o -Wl,-rpath,$HOME/.xacc/lib -L $HOME/.xacc/lib -lxacc `$HOME/.exatn/bin/exatn-config --libs` -o xacc_app.x
+g++ -std=c++17 -I $HOME/.xacc/include/xacc -c xacc_app.cpp -o xacc_app.o
+g++ xacc_app.o -Wl,-rpath,$HOME/.xacc/lib -L $HOME/.xacc/lib -lxacc -o xacc_app.x
